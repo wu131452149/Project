c37b9ff6af6310bfe22359947f232df05feb362c
Lily <Lihong.Li@cn.dlink.com>
2019-10-08 23:33:43 +0800

    去掉备案号

--- src/components/layout/BottomNav.vue
+++ src/components/layout/BottomNav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="footer">
-        <p class="">Copyright © www.XXX.cn All Rights Reserved. 备案号：晋ICP备777777777号-1</p>
+        <!--<p class="">Copyright © www.XXX.cn All Rights Reserved. 备案号：晋ICP备777777777号-1</p>-->
     </div>
 </template>
 
